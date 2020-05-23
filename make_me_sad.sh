976449be2c06b34254d201a008ff1566fa4b5965
Julien Schmidt <julien.schmidt@tum.de>
2020-05-23 19:49:11 +0200

    mms: less verbosity

--- make_me_sad.sh
+++ make_me_sad.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-set -ev
+set -e
 
 # run unit tests and measure coverage
+echo "running unit tests..."
 go test -v -cover -covermode=count
 
 # gofmt check
+echo "checking formatting..."
 test -z "$(gofmt -d -s . | tee /dev/stderr)"
 
 # run linter
-if type "golangci-lint" > /dev/null; then
+echo "running linter..."
+if type "golangci-lintx" &> /dev/null; then
     golangci-lint run --disable varcheck,deadcode,unused
 else
     echo "WARNING: golangci-lint not installed. Only running go vet"
