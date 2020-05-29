a862a689c84391fdd625c5013011a34f479c4b04
Julien Schmidt <julien.schmidt@tum.de>
2020-05-29 23:17:32 +0200

    config: fix test

--- config_test.go
+++ config_test.go
@@ -1,13 +1,36 @@
 package main
 
 import (
-	"github.com/stretchr/testify/assert"
+	"bytes"
+	"io/ioutil"
+	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestConfig_FromFile(t *testing.T) {
+const configFile = "config.conf"
+
+func TestConfigFromFile(t *testing.T) {
+	file, err := ioutil.TempFile("", "test_config")
+	require.Nil(t, err)
+	defer os.Remove(file.Name())
+
+	data, err := ioutil.ReadFile(configFile)
+	require.Nil(t, err)
+
+	// replace hostkey path
+	data = bytes.Replace(data,
+		[]byte(" hostkey.pem"),
+		[]byte(" .testing/hostkey.pem"),
+		1)
+
+	err = ioutil.WriteFile(file.Name(), data, 0644)
+	require.Nil(t, err)
+
 	config := Config{}
-	err := config.FromFile("config.conf")
+	err = config.FromFile(file.Name())
 
 	assert.Nil(t, err)
 }
