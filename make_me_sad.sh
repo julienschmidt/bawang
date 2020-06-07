8f09449a860e2a8e4bb88d13bd4d712cabd9049d
Julien Schmidt <julien.schmidt@tum.de>
2020-06-07 16:31:24 +0200

    setup Gitlab CI

--- make_me_sad.sh
+++ make_me_sad.sh
@@ -13,7 +13,7 @@ test -z "$(gofmt -d -s . | tee /dev/stderr)"
 # run linter
 echo "running linter..."
 if type "golangci-lint" &> /dev/null; then
-    golangci-lint run --enable goconst,prealloc ./...
+    golangci-lint run --enable goconst,golint,maligned,misspell,prealloc,stylecheck ./...
 else
     echo "WARNING: golangci-lint not installed. Only running go vet"
     echo "See: https://golangci-lint.run/usage/install/#local-installation"
